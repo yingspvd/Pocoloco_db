82ed7567fe76153eaf45fcf1c7ba93b81854eb5e
yingspvd <supavadeeying@outlook.com>
2021-04-14 00:31:35 +0700

    BookingDetail - Get RoomNumber

---
+++ api_booking.php
@@ -0,0 +1,76 @@
+<?php 
+require_once 'connect.php';
+$request_data=json_decode(file_get_contents("php://input"));
+$out = array();
+
+if($request_data -> action == "getBookingID"){
+  
+  // query
+  $sql = "SELECT MAX(bookingID) AS bookingID FROM booking";
+  $query = $connect->query($sql);
+  
+  while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+      $data[] = $row;
+  }
+
+  // Set BookingID
+  if($data[0]["bookingID"] == ""){
+    $bookingID = 1000000001;
+  }
+  else{
+    $bookingID = $data[0]["bookingID"] + 1;
+  }
+  
+  echo json_encode($bookingID);
+}
+
+
+if($request_data -> action == "addBooking"){
+  
+  // Input from USER
+  $bookingID = $request_data -> bookingID;
+  $customerID = $request_data -> customerID;
+
+  // // Check have CustomerID
+  // $sql = "SELECT customerID 
+  //         FROM customer
+  //         GROUP BY customerID
+  //         HAVING customerID = $customerID"
+  
+  // $query = $connect->query($sql);
+
+  // if($query->rowCount() == 1){
+  //   // Add Booking to DB
+  //   $sql = "INSERT INTO booking
+  //   VALUES ('$bookingID','$customerID')";
+
+  //   $query = $connect->query($sql);
+
+  //   if($query){
+  //     $out['message'] = "Added Successfully";
+  //   }
+  //   else{
+  //     $out['message'] = "Could not add";
+  //   }
+    
+  // }
+
+  // else{
+  //   $out['message'] = "Don't have this customer ID"
+  // }
+  
+  $sql = "INSERT INTO booking
+    VALUES ('$bookingID','$customerID')";
+
+    $query = $connect->query($sql);
+
+    if($query){
+      $out['message'] = "Added Successfully";
+    }
+    else{
+      $out['message'] = "Could not add";
+    }
+    
+  echo json_encode($out);
+
+}
---
+++ api_bookingDetail.php
@@ -0,0 +1,148 @@
+<?php 
+require_once 'connect.php';
+$request_data=json_decode(file_get_contents("php://input"));
+$allRoom = array();
+
+if($request_data -> action == "getRoomType"){
+  
+  // query
+  $sql = "SELECT roomTypeID,roomType FROM roomdescription";
+  $query = $connect->query($sql);
+  
+  while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+      $data[] = $row;
+  }
+
+  echo json_encode($data);
+}
+
+if($request_data -> action == "getRoomNumber"){
+  
+  $roomType = $request_data -> roomType;
+  $checkIn = $request_data -> checkIn;
+  $checkOut = $request_data -> checkOut;
+
+  $dataCheck = $checkIn;
+  
+  
+    while($dataCheck <= $checkOut){
+    // query
+    $sql = "SELECT roomID 
+    FROM bookingdetail 
+    WHERE roomID IN
+      (SELECT roomID 
+      FROM hotelroom
+      WHERE roomTypeID = '$roomType' AND 
+            (status = 'R' OR status = 'I') AND
+            ('$dataCheck' BETWEEN checkIn AND checkOut))";
+            
+    $query = $connect->query($sql);
+
+    while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+    $data[] = $row["roomID"];
+    }
+
+    array_push($allRoom,$data);
+    $dataCheck = date('Y-m-d',strtotime($dataCheck. '1 days'));
+    }
+   
+    $str_arr = explode(",",$allRoom);
+    $result = array_unique($str_arr);
+    
+   echo json_encode($result);
+}
+
+if($request_data -> action == "addBookingDetail"){
+    
+    function check_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+      
+    // From User
+    $bookingID = $request_data -> bookingID;
+    $guestFirstName = check_input($request_data -> guestFirstName);
+    $guestLastName = check_input($request_data -> guestLastName);
+    $roomID = $request_data -> roomNumber;
+    $roomID = 1202;
+    $checkIn = $request_data -> checkIn;
+    $checkOut = $request_data -> checkOut;
+
+    // Check GuestName
+    if($guestFirstName==''){
+      $out['message'] = "Guest Name is required";
+    }
+      
+    // Check Guest Surname
+    else if($guestLastName ==''){
+      $out['message'] = "Guest Surname is required";
+    }
+    
+    // Check RoomType
+    else if($roomID ==''){
+      $out['message'] = "Room Number is required";
+    }
+
+    // Check Check in date
+    else if($checkIn ==''){
+      $out['message'] = "Check in date is required";
+    }
+
+    // Check Check out date
+    else if($checkOut ==''){
+      $out['message'] = "Check out date is required";
+    }
+
+    else{
+
+      // query BookingID
+      $sql = "SELECT MAX(bookingDetailID) AS bookingDetailID FROM bookingdetail";
+      $query = $connect->query($sql);
+      
+      while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+          $data[] = $row;
+      }
+
+      // Set BookingID
+      if($data[0]["bookingDetailID"] == ""){
+        $bookingDetailID = 1000000001;
+      }
+      else{
+        $bookingDetailID = $data[0]["bookingDetailID"] + 1;
+      }
+      
+      // Set Name
+      $guestFirstName = ucfirst($guestFirstName);
+      $guestLastName = ucfirst($guestLastName);
+      $bookingID = intval($bookingID);
+      
+      $out['bookingDetailID'] = $bookingDetailID;
+      $out['bookingID'] = $bookingID;
+      $out['checkIn'] = $checkIn;
+      $out['checkOut'] = $checkOut;
+      $out['guestFirstName'] = $guestFirstName;
+      $out['guestLastName'] = $guestLastName;
+
+      
+      // Add Booking Detail in DB
+      $sql = "INSERT INTO bookingdetail(`bookingDetailID`, `bookingID`, `roomID`, `checkIn`, `checkOut`, `guestFirstName`, `guestLastName`, `status`, `dateTime`) 
+              VALUES ('$bookingDetailID','$bookingID','$roomID','$checkIn','$checkOut','$guestFirstName','$guestLastName','R',CURRENT_TIMESTAMP)";
+      $query = $connect->query($sql);
+
+      if($query){
+        $out['message'] = "User Added Successfully";
+        $out['Success'] = 1;
+      }
+      else{
+        $out['error'] = true;
+        $out['message'] = "Could not add";
+      }
+    }
+    echo json_encode($out);
+}
+
+
+
+?>
