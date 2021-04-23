3de8272576ed6577dffed8867c07728bc2f30c2a
yingspvd <supavadeeying@outlook.com>
2021-04-24 01:38:51 +0700

    customer

--- api_customer.php
+++ api_customer.php
@@ -40,3 +40,61 @@ if($request_data->action=="searchData"){
     
     echo json_encode($data);  
 }
+
+if($request_data -> action == "editData"){
+    
+    $customerID = $request_data -> customerID;
+    
+    $sql = "SELECT *
+            FROM customer
+            WHERE customerID = '$customerID' ";
+    
+    $query = $connect->query($sql);
+    
+    while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+        $data[] = $row;
+    }
+    
+    echo json_encode($data);  
+}
+
+if($request_data -> action == "updateData"){
+    $customerID = $request_data -> customerID;
+    $firstName = $request_data -> firstName;
+    $lastName = $request_data -> lastName;
+    $DOB = $request_data -> DOB;
+    $phone = $request_data -> phone;
+    $email = $request_data -> email;
+    $address = $request_data -> address;
+
+    $sql = "UPDATE customer 
+            SET firstName = '$firstName', lastName = '$lastName', DOB = '$DOB',
+                phone = '$phone', email = '$email' , address = '$address'
+            WHERE customerID = '$customerID'";
+    $query = $connect->query($sql);
+
+    if($query){
+        $out['message'] = "Updated Successfully";
+        $out['success'] = true;
+        }
+        else{
+        $out['message'] = "Could not add ";
+        }
+
+    echo json_encode($out); 
+}
+
+if($request_data -> action == "deleteData"){
+    $sql = "DELETE FROM customer WHERE customerID = $request_data->customerID";
+    $query = $connect->query($sql);
+
+    if($query){
+        $out['message'] = "Deleted Successfully";
+        $out['success'] = true;
+        }
+    else{
+        $out['message'] = "Could not delete ";
+    }
+
+    echo json_encode($out); 
+}
