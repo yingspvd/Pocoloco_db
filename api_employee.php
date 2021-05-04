00babd9f620ab9049f11b0fd4f2eae6eebd0d5ad
yingspvd <supavadeeying@outlook.com>
2021-05-04 23:23:49 +0700

    Employee - Search

--- api_employee.php
+++ api_employee.php
@@ -4,11 +4,12 @@ $request_data=json_decode(file_get_contents("php://input"));
 
 if($request_data -> action == "getEmployee"){
     $sql = "SELECT e.employeeID,e.em_firstname,e.em_lastname,d.departmentName,
-                    r.roleName,r.salary, e.workStatus,e.shift,e.startDate,
-                    e.identification,e.gender,e.DOB,e.email,e.phone
+            r.roleName,r.salary, e.workStatus,e.shift,e.startDate,
+            e.identification,e.gender,e.DOB,e.email,e.phone,
+            (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM e.startDate)) AS 'duration'
             FROM employee e,department d,role r
             WHERE e.department = d.departmentID AND
-                    e.roleID = r.roleID";
+                  e.roleID = r.roleID";
                     
     $query = $connect->query($sql);
     
@@ -51,7 +52,7 @@ if($request_data-> action == "getRole")
 
 }
 
-if($request_data -> action == "saveEdit"){
+if($request_data -> action == "updateData"){
     $employeeID = intval($request_data -> employeeID);
     $department = $request_data -> department;
     $role = $request_data -> role;
@@ -65,43 +66,101 @@ if($request_data -> action == "saveEdit"){
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $out['message'] = "Invalid Email Format";
     }
-
-    $sql = "SELECT d.departmentID,r.roleID 
-            FROM department d,role r 
-            WHERE d.departmentID = (SELECT departmentID 
-                                    FROM department 
-                                    WHERE departmentName = '$department') 
-                    AND
-                    r.roleID = (SELECT roleID 
-                                FROM role 
-                                WHERE roleName = '$role')";
-
-    $query = $connect->query($sql);
-        
-    while($row = $query -> fetch(PDO::FETCH_ASSOC)){
-        $department = $row['departmentID'];
-        $roleID = $row['roleID'];
-    }
-
-
-    $sql_insert = "UPDATE employee 
-                    SET department = '$department', roleID = '$roleID', shift = '$shift', 
-                    em_firstname = '$firstName', em_lastname = '$lastName' , phone = '$phone',
-                    email = '$email', workStatus = '$workStatus'
-                    WHERE employeeID = '$employeeID'";
-    
-    $query = $connect->query($sql_insert);
-
-    if($query){
-        $out['message'] = "Updated Successfully";
-        $out['success'] = true;
-    }
     else{
-        $out['message'] = "Could not update";
-        $out['success'] = false;
+
+        $sql = "SELECT d.departmentID,r.roleID 
+                FROM department d,role r 
+                WHERE d.departmentID = (SELECT departmentID 
+                                        FROM department 
+                                        WHERE departmentName = '$department') 
+                        AND
+                        r.roleID = (SELECT roleID 
+                                    FROM role 
+                                    WHERE roleName = '$role')";
+
+        $query = $connect->query($sql);
+            
+        while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+            $department = $row['departmentID'];
+            $roleID = $row['roleID'];
+        }
+
+
+        $sql_insert = "UPDATE employee 
+                        SET department = '$department', roleID = '$roleID', shift = '$shift', 
+                        em_firstname = '$firstName', em_lastname = '$lastName' , phone = '$phone',
+                        email = '$email', workStatus = '$workStatus'
+                        WHERE employeeID = '$employeeID'";
+        
+        $query = $connect->query($sql_insert);
+
+        if($query){
+            $out['message'] = "Updated Successfully";
+            $out['success'] = true;
+        }
+        else{
+            $out['message'] = "Could not update";
+            $out['success'] = false;
+        }
     }
-    
     echo json_encode($out);
 }
 
+if($request_data -> action == "searchData"){
+    $search = $request_data -> search;
+    $filter = $request_data -> filter;
+    $sort = $request_data -> sort;
+
+    if($filter == "all" && $sort == "all"){
+        $sql = "SELECT * FROM employee_view
+                WHERE employeeID LIKE '$search%' OR 
+                    em_firstname LIKE '$search%' OR 
+                    departmentName LIKE '$search%' OR 
+                    roleName LIKE '$search%' OR 
+                    workStatus LIKE '$search%' OR 
+                    salary LIKE '$search%' OR 
+                    duration LIKE '$search%' 
+                    ORDER BY employeeID,departmentName,em_firstname,
+                            roleName,workStatus,salary DESC,duration DESC
+                ";
+    }
+    elseif($filter != "all" && $sort == "all"){
+        $sql = "SELECT * FROM employee_view
+        WHERE $filter LIKE '$search%' 
+        ORDER BY employeeID,departmentName,em_firstname,
+                roleName,workStatus,salary DESC,duration DESC
+        ";
+    }
+    elseif($filter != "all" && $sort == "salary"){
+        $sql = "SELECT * FROM employee_view
+        WHERE $filter LIKE '$search%' 
+        ORDER BY salary DESC
+        ";
+    }
+    elseif($filter != "all" && $sort == "duration"){
+        $sql = "SELECT * FROM employee_view
+                WHERE $filter LIKE '$search%' 
+                ORDER BY duration DESC
+                ";
+    }
+    else{
+        $sql = "SELECT * FROM employee_view
+                WHERE $filter LIKE '$search%' 
+                ORDER BY $sort
+                "; 
+    }
+
+    $query = $connect->query($sql);
+            
+    while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+        $data[] = $row; 
+    }
+
+    if($query->rowCount() == 0)
+    {
+        $data = "";
+    } 
+    
+    echo json_encode($data);
+}
 ?>
