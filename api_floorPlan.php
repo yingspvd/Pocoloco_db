536c444d1c4690d20a5659839e0936c837ff531e
yingspvd <supavadeeying@outlook.com>
2021-06-02 23:04:58 +0700

    Floor Plan API

---
+++ api_floorPlan.php
@@ -0,0 +1,32 @@
+<?php 
+require_once 'connect.php';
+$request_data=json_decode(file_get_contents("php://input"));
+
+if($request_data -> action == "getRoomNumber"){
+
+    $building = intval($request_data -> building);
+    $floor = intval($request_data -> floor);
+
+  
+    // query
+    $sql = "SELECT h.roomID,r.roomType,
+                CASE
+                WHEN  b.status = 'O' THEN 1
+                WHEN  b.status = 'C' THEN 1
+                WHEN  b.status = 'I' THEN 0
+                WHEN  b.status = 'R' THEN 0 
+                ELSE  1
+                END AS status
+            FROM hotelroom h LEFT JOIN bookingdetail b ON h.roomID = b.roomID
+            LEFT JOIN roomdescription r ON h.roomTypeID = r.roomTypeID
+            WHERE h.roomID LIKE '$building$floor%' ";
+    $query = $connect->query($sql);
+    
+    while($row = $query -> fetch(PDO::FETCH_ASSOC)){
+        $data[] = $row;
+    }
+  
+    echo json_encode($data);
+  }
+
+?>
