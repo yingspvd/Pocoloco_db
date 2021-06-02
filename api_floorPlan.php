1f60b14b7481e72e1f20944326c1a1a85a060451
yingspvd <supavadeeying@outlook.com>
2021-06-03 02:05:46 +0700

    Floorplan

--- api_floorPlan.php
+++ api_floorPlan.php
@@ -6,8 +6,8 @@ if($request_data -> action == "getRoomNumber"){
 
     $building = intval($request_data -> building);
     $floor = intval($request_data -> floor);
-
-  
+    $date = $request_data -> date;
+       
     // query
     $sql = "SELECT h.roomID,r.roomType,
                 CASE
@@ -17,9 +17,10 @@ if($request_data -> action == "getRoomNumber"){
                 WHEN  b.status = 'R' THEN 0 
                 ELSE  1
                 END AS status
-            FROM hotelroom h LEFT JOIN bookingdetail b ON h.roomID = b.roomID
-            LEFT JOIN roomdescription r ON h.roomTypeID = r.roomTypeID
-            WHERE h.roomID LIKE '$building$floor%' ";
+            FROM hotelroom h LEFT JOIN bookingdetail b ON h.roomID = b.roomID AND ('$date' BETWEEN b.checkIn AND b.checkOut)
+            LEFT JOIN roomdescription r ON h.roomTypeID = r.roomTypeID 
+            WHERE h.roomID LIKE '$building$floor%'";
+                
     $query = $connect->query($sql);
     
     while($row = $query -> fetch(PDO::FETCH_ASSOC)){
