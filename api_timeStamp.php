b2ed2d8977b653afaba9aed7de4f448c2f0418e8
yingspvd <supavadeeying@outlook.com>
2021-06-04 05:42:32 +0700

    Add check late

--- api_timeStamp.php
+++ api_timeStamp.php
@@ -6,8 +6,33 @@ if($request_data -> action == 'timeStamp'){
     $employeeID = intval($request_data -> employeeID) ;
     $type = $request_data -> type;
     
-    $sql = "INSERT INTO timestamp (employeeID,type)
-            VALUES('$employeeID','$type')";
+    $sql = "SELECT shift FROM `employee` WHERE employeeID = $employeeID";
+    $query = $connect->query($sql);
+    while($row = $query->fetch(PDO::FETCH_ASSOC)){
+        $shift =$row["shift"];
+    }
+
+    if($shift == 1){
+        $sql = "SELECT  CAST(CURRENT_TIMESTAMP as time) >= '03:00:00' AND 
+                CAST(CURRENT_TIMESTAMP as time) < '05:00:00' AS checklate";
+    }
+    else if($shift == 2){
+        $sql = "SELECT  CAST(CURRENT_TIMESTAMP as time) >= '11:00:00' AND 
+                CAST(CURRENT_TIMESTAMP as time) < '13:00:00' AS checklate";
+    }
+    else if($shift == 3){
+        $sql = "SELECT  CAST(CURRENT_TIMESTAMP as time) >= '19:00:00' AND 
+                CAST(CURRENT_TIMESTAMP as time) < '21:00:00' AS checklate";
+    }
+    $query = $connect->query($sql);
+    while($row = $query->fetch(PDO::FETCH_ASSOC)){
+        $checklate =$row["checklate"];
+    }
+
+    $checklate = intval($checklate);
+
+    $sql = "INSERT INTO timestamp (employeeID,type,late)
+            VALUES('$employeeID','$type','$checklate')";
 
     $query = $connect->query($sql);   
     
